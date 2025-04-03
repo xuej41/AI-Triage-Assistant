f8276e1570dd773ec1a79f4144abc02c473c7db2
xuej41 <121536584+xuej41@users.noreply.github.com>
2025-04-02 20:41:51 -0400

    Update app

--- README.md
+++ README.md
@@ -3,20 +3,19 @@
 This repository contains the code to run an AI-powered assistant to provide medical advice, answer patient queries, and triage symptoms, enhancing patient engagement and care.
 
 
-
 ## Preview
 <img src="static/preview.png" alt="app preview">
 
 ## Overview
 
-The project focuses providing patients with medical advice, answer patient queries, and triage symptoms, enhancing patient engagement with care using a virtual assistant.
+This Streamlit application focuses providing patients with medical advice, answer patient queries, and triage symptoms, enhancing patient engagement with care using a virtual triage assistant.
+Created for the ENG 2PX3 Sprint 3 Project.
 
 ## Project Structure
 
 The project is structured as follows:
 
-- `virtual_doc.py`: Application to run the virtual assistant.
-- `Dockerfile`: Docker configuration for containerizing the application.
+- `app.py`: Application to run the triage assistant.
 - `requirements.txt`: File containing the necessary dependencies to run the project.
 
 
@@ -26,17 +25,22 @@ To get started with the project, follow these steps:
 
 1. Clone this repository to your local machine.
     ```bash
-    git clone https://github.com/RiteshYennuwar/Virtual_HealthCare_Assistant.git
-    cd Virtual_HealthCare_Assistant
+    git clone https://github.com/xuej41/AI-Triage-Assistant.git
+    cd AI-Triage-Assistant
 2. Install the required dependencies using 
     ```bash
     pip install -r requirements.txt
+3. If you get an error involving `dotenv` not being found, run the following command in your terminal:
+    ```bash
+    python -m pip install python-dotenv
+3. Head to https://groq.com/groqcloud/, login or make an account, and obtain your API key.
+4. Create a `.env` file and paste the line `API_KEY='Your API key here'` (replace Your API key here with your API key).
 
 ## Usage
 
-- **Application**:  To start the application and use the virtual assistant execute the following command:
+- **Application**:  To start the application and use the virtual triage assistant execute the following command:
     ```bash
-    streamlit run virtual_doc.py
+    streamlit run app.py
 
 After running the command, navigate to http://localhost:8501/ in your web browser to access the application.
 
---
+++ app.py
@@ -0,0 +1,102 @@
+import streamlit as st
+from typing import Generator
+from textblob import TextBlob
+from groq import Groq
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+st.set_page_config(page_icon="🏥", layout="wide", page_title="Triage Assistant")
+
+def icon(emoji: str):
+    """Shows an emoji as a Notion-style page icon."""
+    st.write(f'<span style="font-size: 78px; line-height: 1">{emoji}</span>', unsafe_allow_html=True)
+
+icon("AI Triage Assistant 🚑🚨🏥")
+
+client = Groq(
+    api_key= os.getenv('API_KEY'), # groq cloud
+)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+def analyze_mood(text):
+    analysis = TextBlob(text)
+    return "Positive" if analysis.sentiment.polarity > 0 else "Negative" if analysis.sentiment.polarity < 0 else "Neutral"
+
+def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
+    """Yield chat response content from the Groq API response."""
+    for chunk in chat_completion:
+        if chunk.choices[0].delta.content:
+            yield chunk.choices[0].delta.content
+
+st.header("Group 122's AI-Powered Triage System Client")
+st.write("Please input the patient's symptoms for the AI to diagnose.")
+
+# Layout with two columns
+col1, col2 = st.columns([1, 1])  # Left column (3 parts) and right column (1 part)
+# Left column content
+
+# Display chat history
+with col1:
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+# Real-time chat input
+if prompt := st.chat_input("Voice activation: Start speaking..."):
+    # Display user message
+    with col1:
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        try:
+            chat_completion = client.chat.completions.create(
+                model="gemma2-9b-it",
+                messages=[
+                    {
+                        "role": "user",
+                        #"content": "You are a virtual doctor named DocOuc. Your role is to diagnose and provide medical advice to patients while asking as few questions as possible. Your approach should be friendly, empathetic, and supportive, offering not only medical insights but also emotional and moral support. Engage with the patient in a kind and understanding manner, making them feel comfortable and cared for. Aim to build trust and provide reassurance throughout the conversation.Analyze the problem and provide the solution in a clear and concise manner.Do not ask too many questions, provide the tratment initally and then proceed with other things . Try to build the conversation and help out in a friendly manner. The response must be broken down into points."
+                        "content": "You are a triage assistant working alongside a nurse in emergency response. Your role is to diagnose and provide medical advice to the nurse based on the patient's description of their injury. Your answers should be concise and listed as points. Try and list several injury/disease possibilities based on what the patient describes, and list the probability of the patient suffering from said injury/disease. Aim to narrow down the list as the patient provides more context. At the end of each response, rate the severity of the patient's condition on a scale of 1-10, with 10 being the most severe, in this format: Patient Priority Level: X/10."
+                    },
+                    {
+                        "role": "assistant",
+                        #"content": "Hello there! I'm DocOuc, your virtual doctor friend. It's wonderful to meet you! I'm here to listen, understand, and help you with any health concerns you may have. Please know that everything discussed in this chat is completely confidential and judgement-free. You're in a safe space now.\n\nWhat brings you to my virtual clinic today? Is there something specific that's been bothering you lately, or do you just need some general guidance on how to take care of yourself?"
+                    },
+                    {"role": "user", "content": prompt}
+                ],
+                max_tokens=1028,
+                stream=True
+            )
+
+            # Display AI response
+            with st.chat_message("assistant"):
+                chat_responses_generator = generate_chat_responses(chat_completion)
+                full_response = ''.join(list(chat_responses_generator))
+                st.markdown(full_response)
+                # mood_analysis = analyze_mood(prompt)
+                # st.write("Triage Priority Analysis:", mood_analysis)
+
+            st.session_state.messages.append({"role": "doctor", "content": full_response})
+        except Exception as e:
+            st.error(e, icon="🚨")
+    
+
+# Right column content (Patient's Info)
+with col2:
+    st.header("Patient Information")
+    st.write("**Name:** John Doe")
+    st.write("**Age:** 35")
+    st.write("**Gender:** Male")
+    st.write("**Medical History:**")
+    st.write("- Hypertension")
+    st.write("- Diabetes")
+    st.write("- High Blood Pressure")
+    
+    # Add a selection box for Priority
+    priority = st.selectbox("Priority", options=[""] + list(range(1, 11)), index=0)
+    st.subheader(f"Selected Priority: {priority}")
